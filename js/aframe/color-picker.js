57e0af6031e0877af1ef90861f9056449e8638f0
d-asnaghi <asnaghi@me.com>
2020-11-16 23:03:35 -0500

    [PICKER] support texture and shape

--- js/aframe/color-picker.js
+++ js/aframe/color-picker.js
@@ -1,70 +1,113 @@
+AFRAME.registerComponent("color-picker", {
+  dependencies: ["hand-tracking-mesh"],
 
-AFRAME.registerComponent('color-picker', {
+  init: function () {
+    let setRoomSides = function (room, sides) {
+      walls = room.querySelector("#walls");
+      roof = room.querySelector("#roof");
+    //   walls.setAttribute("material", {
+    //     repeat: sides,
+    //   });
+      walls.setAttribute("segments-radial", sides);
+      roof.setAttribute("segments", sides);
+    };
 
-    dependencies: ['hand-tracking-mesh'],
+    let setRoomTexture = function (room, texture) {
+      walls = room.querySelector("#walls");
+      roof = room.querySelector("#roof");
+      walls.setAttribute("material", {
+        src: texture,
+      });
+      roof.setAttribute("material", {
+        src: texture,
+      });
+    };
 
-    init: function() {
+    let roomPicker = function (ctx, texture, sides) {
+      let picker = document.createElement("a-cylinder");
+      picker.setAttribute("radius", 0.02);
+      picker.setAttribute("height", 0.005);
+      picker.setAttribute("segments-radial", sides);
+      picker.setAttribute("material", {
+        color: "white",
+        src: texture,
+      });
+      picker.addEventListener("raycaster-intersected", (evt) => {
+        picker.setAttribute("scale", "1.2 1.2 1.2");
+        setRoomTexture(ctx.room, texture);
+        setRoomSides(ctx.room, sides);
+      });
+      picker.addEventListener("raycaster-intersected-cleared", (evt) => {
+        picker.setAttribute("scale", "1 1 1");
+      });
+      ctx.el.sceneEl.appendChild(picker);
+      return picker;
+    };
 
-        let picker = function(ctx, texture, color) {
-            let picker = document.createElement("a-box");
-            picker.setAttribute("width", 0.02);
-            picker.setAttribute("height", 0.02);
-            picker.setAttribute("depth", 0.02);
-            picker.setAttribute("material", {
-                color: "white",
-                src: texture
-            });
-            picker.addEventListener('raycaster-intersected', evt => {
-                picker.setAttribute('scale', '1.2 1.2 1.2');
-                ctx.painter.setAttribute('painter', {
-                    color: color
-                });
-                ctx.room.setAttribute("material", {
-                    src: texture
-                });
-            });
-            picker.addEventListener('raycaster-intersected-cleared', evt => {
-                picker.setAttribute('scale', '1 1 1');
-            });
-            ctx.el.sceneEl.appendChild(picker);
-            return picker
-        }
+    this.handTracking = this.el.components["hand-tracking-mesh"];
 
-        this.handTracking = this.el.components['hand-tracking-mesh'];
+    this.painter = document.querySelector("[painter]");
+    this.room = document.querySelector("#room");
 
-        this.painter = document.querySelector("[painter]");
-        this.room = document.querySelector("#room");
+    this.picker1 = roomPicker(this, "textures/tiles.jpg", 3);
+    this.picker2 = roomPicker(this, "textures/hexagon.jpg", 6);
+    this.picker3 = roomPicker(this, "textures/mosaic.jpg", 8);
+  },
 
-        this.picker1 = picker(this, "textures/triangle.jpg", "white");
-        this.picker2 = picker(this, "textures/hexagon.jpg", "red");
-        this.picker3 = picker(this, "textures/mosaic.jpg", "black");
-    },
+  tick: function () {
+    if (this.handTracking.jointsLoaded() && this.handTracking.mesh != null) {
+      const joint = this.handTracking.getJoints()[0];
 
-    tick: function() {
-        if (this.handTracking.jointsLoaded() && this.handTracking.mesh != null) {
-            const joint = this.handTracking.getJoints()[0]
+      this.picker1.setAttribute("visible", joint.visible);
+      this.picker1.object3D.position.copy(
+        joint.position.clone().add(new THREE.Vector3(0, 1.5, 0))
+      );
+      this.picker1.object3D.quaternion.copy(
+        new THREE.Quaternion(
+          joint.orientation.x,
+          joint.orientation.y,
+          joint.orientation.z,
+          joint.orientation.w
+        )
+      );
+      this.picker1.object3D.rotateY(Math.PI / 2);
+      this.picker1.object3D.translateX(-0.1);
+      this.picker1.object3D.translateY(0.01);
+      this.picker1.object3D.translateZ(0.05);
 
-            this.picker1.setAttribute('visible', joint.visible);
-            this.picker2.setAttribute('visible', joint.visible);
-            this.picker3.setAttribute('visible', joint.visible);
+      this.picker2.setAttribute("visible", joint.visible);
+      this.picker2.object3D.position.copy(
+        joint.position.clone().add(new THREE.Vector3(0, 1.5, 0))
+      );
+      this.picker2.object3D.quaternion.copy(
+        new THREE.Quaternion(
+          joint.orientation.x,
+          joint.orientation.y,
+          joint.orientation.z,
+          joint.orientation.w
+        )
+      );
+      this.picker2.object3D.rotateY(Math.PI / 2);
+      this.picker2.object3D.translateZ(0.05);
+      this.picker2.object3D.translateY(0.01);
+      this.picker2.object3D.translateX(-0.15);
 
-            this.picker1.object3D.position.copy(joint.position.clone().add(new THREE.Vector3(0, 1.5, 0)));
-            this.picker1.object3D.quaternion.copy(new THREE.Quaternion(joint.orientation.x, joint.orientation.y, joint.orientation.z, joint.orientation.w));
-            this.picker1.object3D.rotateY(Math.PI / 2);
-            this.picker1.object3D.translateZ(-0.04);
-
-            this.picker2.object3D.position.copy(joint.position.clone().add(new THREE.Vector3(0, 1.5, 0)));
-            this.picker2.object3D.quaternion.copy(new THREE.Quaternion(joint.orientation.x, joint.orientation.y, joint.orientation.z, joint.orientation.w));
-            this.picker2.object3D.rotateY(Math.PI / 2);
-            this.picker2.object3D.translateZ(-0.07);
-
-            this.picker3.object3D.position.copy(joint.position.clone().add(new THREE.Vector3(0, 1.5, 0)));
-            this.picker3.object3D.quaternion.copy(new THREE.Quaternion(joint.orientation.x, joint.orientation.y, joint.orientation.z, joint.orientation.w));
-            this.picker3.object3D.rotateY(Math.PI / 2);
-            this.picker3.object3D.translateZ(-0.1);
-
-        }
-
+      this.picker3.setAttribute("visible", joint.visible);
+      this.picker3.object3D.position.copy(
+        joint.position.clone().add(new THREE.Vector3(0, 1.5, 0))
+      );
+      this.picker3.object3D.quaternion.copy(
+        new THREE.Quaternion(
+          joint.orientation.x,
+          joint.orientation.y,
+          joint.orientation.z,
+          joint.orientation.w
+        )
+      );
+      this.picker3.object3D.rotateY(Math.PI / 2);
+      this.picker3.object3D.translateX(-0.2);
+      this.picker3.object3D.translateY(0.01);
+      this.picker3.object3D.translateZ(0.05);
     }
-
+  },
 });
